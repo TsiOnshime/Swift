d3dbcfa1b6ee321165cea4ce2f1ee81134004180
TsiOnshime <153851690+TsiOnshime@users.noreply.github.com>
2025-04-24 08:32:15 +0300

    Integrated front end and backend second trial

--- front-end/src/pages/SignUp/SignUp.jsx
+++ front-end/src/pages/SignUp/SignUp.jsx
@@ -43,7 +43,7 @@ const Register = () => {
         const data = response.data;
         setUser(data.user);
         localStorage.setItem("token", data.token);
-        navigate("/home");
+        navigate("/signin");
       }
       setEmail("");
       setFirstName("");
