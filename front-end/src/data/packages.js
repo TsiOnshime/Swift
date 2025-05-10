c495c30cc6785674b69fdd47acae5dbdb5379091
selamawit shimeles <162134523+selamawitsh@users.noreply.github.com>
2025-05-10 14:48:08 +0300

    Add files via upload
    adding data file for packages

---
+++ front-end/src/data/packages.js
@@ -0,0 +1,25 @@
+
+const packages = [
+    {
+      id: 1,
+      time: '10 minutes',
+      price: '50 ETB',
+      
+    },
+    {
+      id: 2,
+      time: '30 minutes',
+      price: '100 ETB',
+      
+    },
+    {
+      id: 3,
+      time: '1 hour',
+      price: '250 ETB',
+      
+    },
+  ];
+  
+  export default packages;
+  
+  
